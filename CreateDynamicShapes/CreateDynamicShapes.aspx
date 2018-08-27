62a43a88d5e25c8cdd8b2f912aaf775497d0fa63
karmugilanbj <karmugilan.boopathi@gmail.com>
2018-08-28 00:35:45 +0930

    shapes

---
+++ CreateDynamicShapes/CreateDynamicShapes.aspx
@@ -0,0 +1,178 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CreateDynamicShapes.aspx.cs" Inherits="CreateDynamicShapes.CreateDynamicShapes" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Create Dynamic Shapes</title>
+    <script src="Scripts/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript">
+        var canvas;
+        var ctx;
+       function drawRectangle(width,height) {
+            /*InitializeCanvas();*/
+            canvas = document.getElementById('canvas');
+            ctx = canvas.getContext("2d");
+            ctx.fillStyle = '#3AEEFC';
+            ctx.rect(500,250, width, height);
+            ctx.fillRect(500, 250, width, height);
+            ctx.stroke();
+        }
+        function drawCircle(radius) {
+          //Draw a circle
+            canvas = document.getElementById('canvas');
+            ctx = canvas.getContext("2d");
+            ctx.arc(500, 250, radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = '#3AEEFC';
+            ctx.fill();
+            ctx.stroke();
+        }
+         function drawOval(radius) {
+          //Draw an oval
+            canvas = document.getElementById('canvas');
+            ctx = canvas.getContext("2d");
+            var xCenter = parseInt(canvas.width/2);
+            var yCenter = parseInt(canvas.height/2);
+            ctx.translate(xCenter, yCenter);
+            ctx.scale(2, 1);
+            ctx.arc(50, 50, radius, 0, 2 * Math.PI, false);
+            ctx.fillStyle = '#3AEEFC';
+            ctx.fill();
+            ctx.restore(); 
+            ctx.stroke();
+
+        }
+        function drawPolygon(sides, radius) {
+            canvas = document.getElementById('canvas');
+            ctx = canvas.getContext("2d");
+            var noofsides = parseInt(sides);
+            var pradius = parseInt(radius);
+            // Get our canvas center point to center the polygon
+            var xCenter = parseInt(canvas.width/2);
+            var yCenter = parseInt(canvas.height / 2);
+            
+            ctx.beginPath();
+            // Map the first vertice to start with
+            var xPos = xCenter + pradius * Math.cos(2 * Math.PI * 0 / noofsides);
+            var yPos = yCenter + pradius * Math.sin(2 * Math.PI * 0 / noofsides);
+            ctx.moveTo(xPos, yPos);
+            // Loop through the vertices and map the lines
+            for (i = 1; i <= noofsides; i++) {
+                // Determine the coordinates of the next vertex
+                xPos = xCenter + pradius * Math.cos(2 * Math.PI * i / noofsides);
+                yPos = yCenter + pradius * Math.sin(2 * Math.PI * i / noofsides);
+                // Set line to the next vertex
+                ctx.lineTo(xPos, yPos);
+            }
+            ctx.fillStyle = '#3AEEFC';
+            ctx.fill();
+            ctx.stroke();
+
+        }
+
+        function drawParallelogram(width) {
+            canvas = document.getElementById('canvas');
+            ctx = canvas.getContext("2d");
+            var x1, x2, x3, y3, x0, y0;
+            x0 = 100;
+            y0 = 100;
+            //var width = 10; //choose your parallelogram width
+            x1 = x0 + width;
+            y1 = y0;
+
+            x2 = x3 + width;
+            y2 = y3;
+            ctx.beginPath();
+            ctx.moveTo(x0,y0);
+            ctx.lineTo(x1,y1);
+            ctx.lineTo(x2,y2);
+            ctx.lineTo(x3,y3);
+            ctx.closePath();
+            ctx.fillStyle = '#3AEEFC';
+            ctx.fill();
+            ctx.stroke();
+        }
+
+        function drawEquilateral(side) {
+            canvas = document.getElementById('canvas');
+            ctx = canvas.getContext("2d");
+            //equilateral triangle
+            var h = side * (Math.sqrt(3) / 2);
+            //ctx.strokeStyle = "#ff0000";
+            var xCenter = parseInt(canvas.width/2);
+            var yCenter = parseInt(canvas.height/2);
+            ctx.save();
+            ctx.translate(xCenter,yCenter);
+            ctx.beginPath();
+            ctx.moveTo(0, -h / 2);
+            ctx.lineTo(-side / 2, h / 2);
+            ctx.lineTo(side / 2, h / 2);
+            ctx.lineTo(0, -h / 2);
+            ctx.fillStyle = '#3AEEFC';
+            ctx.fill(); 
+            ctx.stroke();
+            //ctx.closePath();
+            //ctx.save();
+        }
+
+    </script>
+</head>
+<body>
+    <form id="frmDynamicShapes" runat="server">
+        <div>
+            <img style="border:0px" src="Shapes.png" height="200px" width="100%" alt=""/> 
+        </div>
+        <div>
+            <br />
+        </div>
+        <div>
+            <table>
+                <tr>
+                    <td>
+                    <asp:Label runat="server" Font-Size="Medium">Draw a</asp:Label> 
+                    </td>
+                    <td>
+                    <asp:DropDownList ID="ddlShapes" runat="server" OnSelectedIndexChanged="ddlShapes_SelectedIndexChanged" AutoPostBack="true"></asp:DropDownList>
+                    </td>
+                     <td>
+                    <asp:Label runat="server" ID="lblwith" Visible="false">with a</asp:Label>
+                    </td>
+                    <td>
+                    <asp:Label ID="lblMeasurement" runat="server" Visible="false"></asp:Label>
+                    </td>
+                    <td>
+                    <asp:Label ID="lblOf" runat="server" Visible="false">of</asp:Label>
+                    </td>
+                    <td>
+                    <asp:TextBox ID="txtMeasureMent1" runat="server" Visible="false"></asp:TextBox>
+                    </td>
+                    <td>
+                    <asp:Label runat="server" ID="lblAnd" Visible="false">and</asp:Label>
+                    </td>
+                    <td>
+                    <asp:Label ID="lblMeasurement2" runat="server" Visible="false"></asp:Label>
+                       
+                    </td>
+                    <td>
+                    <asp:TextBox ID="txtMeasureMent2" runat="server" Visible="false"></asp:TextBox>
+                    </td>
+                 </tr>
+                 <tr>
+                    <td>
+                    <asp:Button ID="btnGenerate" runat="server" Text="Generate Shape" OnClick="btnGenerate_Click">
+                    </asp:Button>
+                   </td>
+                </tr>
+             </table>
+            <table>
+                <tr>
+                    <td>
+                        <canvas id="canvas" width="500" height="500"></canvas>
+                    </td>
+                </tr>
+            </table>
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ CreateDynamicShapes/CreateDynamicShapes.aspx.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Text.RegularExpressions;
+
+namespace CreateDynamicShapes
+{
+    public partial class CreateDynamicShapes : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                LoadShapes();
+            }
+            if(!IsPostBack)
+            {
+
+            }
+        }
+
+        private void LoadShapes()
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml(Server.MapPath("DataSource.xml"));
+            ddlShapes.DataSource = ds;
+            ddlShapes.DataTextField = "shapeName";
+            ddlShapes.DataValueField = "shapeid"; 
+            ddlShapes.DataBind();
+
+            ListItem li = new ListItem("Select Shape", "-1");
+            ddlShapes.Items.Insert(0, li);
+        }
+
+        private void PopulateControls()
+        {
+
+        }
+
+        protected void ddlShapes_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            HideAllTextFields();
+
+            string selectedShape = ddlShapes.SelectedItem.Text;
+            string tempMeasurementText = string.Empty;
+            switch (selectedShape)
+            {
+                
+                case "Square":
+                case "Pentagon":
+                case "Hexagon":
+                case "Heptagon":
+                case "Octagon":
+                case "Equilateral Triangle":
+                    lblwith.Visible = true;
+                    lblMeasurement.Visible = true;
+                    txtMeasureMent1.Visible = true;
+                    lblMeasurement.Text = "side length of";
+                    break;
+                case "Scalene Triangle":
+                case "Isoceles Triangle":
+                case "Parallelogram":
+                case "Rectangle":
+                    lblwith.Visible = true;
+                    lblAnd.Visible = true;
+                    lblMeasurement.Visible = true;
+                    txtMeasureMent1.Visible = true;
+                    lblMeasurement2.Visible = true;
+                    txtMeasureMent2.Visible = true;
+                    lblMeasurement.Text = "width of";
+                    lblMeasurement2.Text = "a height of";
+                    break;
+               
+                case "Circle":
+                case "Oval":
+                    lblwith.Visible = true;
+                    lblMeasurement.Visible = true;
+                    txtMeasureMent1.Visible = true;
+                    lblMeasurement.Text= "radius of";
+                    break;
+             
+            }
+        }
+        private void HideAllTextFields()
+        {
+            lblMeasurement.Text = string.Empty;
+            lblMeasurement2.Text = string.Empty;
+            txtMeasureMent1.Text = string.Empty;
+            txtMeasureMent2.Text = string.Empty;
+
+            lblAnd.Visible = false;
+            lblwith.Visible = false;
+            lblMeasurement.Visible = false;
+            txtMeasureMent1.Visible = false;
+            lblMeasurement2.Visible = false;
+            txtMeasureMent2.Visible = false;
+        }
+
+        protected void btnGenerate_Click(object sender, EventArgs e)
+        {
+            int MeasureMent1 = 0;
+            int MeasureMent2 = 0;
+            var regex = @"^[0-9]+$";
+            string selectedShape = ddlShapes.SelectedItem.Text;
+            if (selectedShape == "Select Shape")
+            {
+                string script = "alert(\"Please Select a Shape\");";
+                ScriptManager.RegisterStartupScript(this, GetType(),
+                                      "ServerControlScript", script, true);
+            }
+            Match match1 = Regex.Match(txtMeasureMent1.Text.Trim(), regex, RegexOptions.IgnoreCase);
+            Match match2 = Regex.Match(txtMeasureMent2.Text.Trim(), regex, RegexOptions.IgnoreCase);
+
+            if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()) && match1.Success)
+            {
+                MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+            }
+            else
+            {
+                string refscript1 = "alert(\"Please Enter a Number\");";
+                ScriptManager.RegisterStartupScript(this, GetType(),
+                                      "ServerControlScript", refscript1, true);
+                return;
+            }
+            if (selectedShape=="Rectangle"|| selectedShape == "Scalene Triangle" || selectedShape == "Isoceles Triangle")
+            { 
+                if (!string.IsNullOrEmpty(txtMeasureMent2.Text.Trim()) && match2.Success)
+                {
+                    MeasureMent2 = Convert.ToInt32(txtMeasureMent2.Text.Trim());
+                }
+                else
+                {
+                    string refscript2 = "alert(\"Please Enter a Number\");";
+                    ScriptManager.RegisterStartupScript(this, GetType(),
+                                      "ServerControlScript", refscript2, true);
+                    return;
+                }
+            }
+            switch (selectedShape)
+            {
+                case "Rectangle":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()) && !string.IsNullOrEmpty(txtMeasureMent2.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawRectangle('" + MeasureMent1 + "','" + MeasureMent2 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+                case "Square":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawRectangle('" + MeasureMent1 + "','" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+
+                case "Pentagon":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawPolygon('" + 5 + "','" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+                case "Hexagon":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawPolygon('" + 6 + "','" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+                case "Heptagon":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawPolygon('" + 7 + "','" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+                case "Octagon":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawPolygon('" + 8 + "','" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+
+                case "Scalene Triangle":
+                case "Isoceles Triangle":
+                    break;
+                case "Equilateral Triangle":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawEquilateral('" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+                case "Parallelogram":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawParallelogram('" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+                case "Circle":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawCircle('" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+                case "Oval":
+                    if (!string.IsNullOrEmpty(txtMeasureMent1.Text.Trim()))
+                    {
+                        MeasureMent1 = Convert.ToInt32(txtMeasureMent1.Text.Trim());
+                        ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawOval('" + MeasureMent1 + "');", true);
+                    }
+                    else
+                    {
+                        string script = "alert(\"Please enter a Value\");";
+                        ScriptManager.RegisterStartupScript(this, GetType(),
+                                              "ServerControlScript", script, true);
+                    }
+                    break;
+                    //ScriptManager.RegisterStartupScript(this, this.GetType(), "Generate Shapes", "drawRectangle('" + 200 + "','" + 200 + "');", true);
+
+
+            }
+           
+
+        }
+
+        
+    }
+}
